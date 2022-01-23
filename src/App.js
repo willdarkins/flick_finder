04c1da06ed1a547024e05932b900600563e17ec2
Will Darkins <willdarkins@gmail.com>
2022-01-23 11:48:37 -0800

    added enviornment variable to store API key... then input the API key via template literal variable into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+require('dotenv').config()
+import MovieGrid from './components/MovieGrid'
 
 function App() {
   //holds search results that come back from search
   const [movies, setMovies] = useState([]);
+
+  //calling MovieDB API
+  const movieRequest = () => {
+    const apiKey= process.env.API_KEY
+    const url = `https://api.themoviedb.org/3/search/keyword?api_key=${apiKey}`
+  }
   return (
     <>
-    <h1>Is this working?</h1>
+    <MovieGrid movies={movies}/>
     </>
   );
 }

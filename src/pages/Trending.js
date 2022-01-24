d9e440ddba090b50ae37e22b3e0828e1c8bd6b8d
Will Darkins <willdarkins@gmail.com>
2022-01-23 22:30:11 -0800

    maded first API call variable on Trending.js... need to input more and test... using axios for the first time

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 function Trending() {
-  return <>
+
+  const trendingResponse= async() => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`)
+  }
+
+  return (
+  <>
     <span className='pageTitle'>Trending</span>
-  </>;
+  </>
+  )
 }
 
 export default Trending;

b1002af9e214cba51b5eb6ce3752eb25336b94c9
Will Darkins <willdarkins@gmail.com>
2022-01-23 22:14:57 -0800

    added images for unavailable poster, image and profile queries

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+import unavailable from './images/unavailable.jpg'
+import unavailableLandscape from './images/unavailableLandscape.jpg'
+import noPicture from './images/noPicture.jpg'
+
+export const unavailable = <img src={unavailable} />
+
+export const unavailableLandscape = <img src={unavailableLandscape} />
+
+export const noPicture = <img src={noPicture} />

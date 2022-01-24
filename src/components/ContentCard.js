ef4ae7131344f406cb5c856afef5174635a4a9f0
Will Darkins <willdarkins@gmail.com>
2022-01-24 00:31:30 -0800

    basic ContentCard.js info has been passed and displayed on a card... working on the webpage and looks good

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -17,7 +17,13 @@ function ContentCard({
       <div>
         {/* if poster exists, then populate the template with the api data...
         if not, display the imported unavailable poster */}
-        <img src={poster ? `${img_300}/${poster}` : unavailable } />
+        <img src={poster ? `${img_300}/${poster}` : unavailable } alt={title}/>
+        <span className='title'>{title}</span>
+        {/* conditionally rendering the media_type to render a tv show or movie bassed on the property's value */}
+        <span className='sub-title'> 
+          {media_type === 'tv'? 'Television Series': 'Movie'}
+          <span className='sub-title'>{date}</span>
+          </span>
       </div>
       </CardStyles>
   )
--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,3 @@
+//width placeholders for poster images inside various components
 export const img_300 = "https://image.tmdb.org/t/p/w300";
 export const img_500 = "https://image.tmdb.org/t/p/w500";

87f86afc0cfb3ffcb4af9b370cd1e954fd454150
Will Darkins <willdarkins@gmail.com>
2022-01-24 22:50:15 -0800

    got started on Movies.js page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const movieResponse = async() => {
+  const { data } = await axios.get()
+}
 
 function Movies() {
-  return <>
+  return(
+  <>
   <span className='pageTitle'>Movies</span>
-  </>;
+  </>
+  )
 }
 
 export default Movies;

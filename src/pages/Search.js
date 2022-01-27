476b19be68979542b9921a39557c8d8d3adfe8ec
Will Darkins <willdarkins@gmail.com>
2022-01-26 23:50:53 -0800

    was able to create Search parts and display at top of screen

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button,
+  Tab,
+  Tabs,
+  TextField
+} from "@material-ui/core";
+import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
+
+  const [type, setType] = useState(0);
+
   return <>
-    <span className='pageTitle'>Search</span>
+    <div style={{display: 'flex', margin: '15px 0 '}}>
+      <TextField
+        style={{ flex: 1 }}
+        className="searchBox"
+        label="Search"
+        variant="filled"
+      // onChange={(e) => setSearchText(e.target.value)}
+      />
+      <Button
+        // onClick={fetchSearch}
+        variant="contained"
+        style={{ marginLeft: 10 }}
+      >
+        <SearchIcon />
+      </Button>
+    </div>
   </>;
 }
+
+const SearchStyles = styled.div`
+  .searchParts{
+    display: 'felx';
+    margin: 15px 0;
+    }
+`
 
 export default Search;
